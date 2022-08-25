79251a56dec9c412840411d68abf52f2482a3058
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-08-25 23:46:42 +0530

    Create Print the Elements of a Linked List.c

---
+++ Print the Elements of a Linked List.c
@@ -0,0 +1,24 @@
+
+
+// Complete the printLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+void printLinkedList(SinglyLinkedListNode* head) {
+
+SinglyLinkedListNode* ptr=head;
+    while(ptr!=NULL)
+    {
+        printf("%d\n",ptr->data);
+        ptr=ptr->next;
+    }
+
+}
+
